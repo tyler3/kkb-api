b0839c037bcb98a1933f02c215046c50f11cb58d
Chihaya Taira <prelude371@gmail.com>
2025-11-11 12:56:20 +0900

    disable solid queue

--- config/puma.rb
+++ config/puma.rb
@@ -48,7 +48,7 @@ else
 end
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+# plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
