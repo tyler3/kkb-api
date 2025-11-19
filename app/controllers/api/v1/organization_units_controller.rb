593d787ba5e249cb25bdc45305d42985369e9c38
Chihaya Taira <prelude371@gmail.com>
2025-11-19 18:05:56 +0900

    宛先並び順変更

--- app/controllers/api/v1/organization_units_controller.rb
+++ app/controllers/api/v1/organization_units_controller.rb
@@ -4,7 +4,7 @@ module Api
       before_action :authenticate_api_v1_user!
 
       def index
-        organization_units = OrganizationUnit.hierarchical_all
+        organization_units = OrganizationUnit.visible.order(:sort_order)
         result = organization_units.map { |unit| { id: unit.id, name: unit.name_with_code } }
         render json: result.to_json
       end
